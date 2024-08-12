2b5273f23f5c40e685344ea87ceabc3ff690427a
HemantAntony <hemantantony@outlook.com>
2024-08-12 20:27:49 +0530

    Created question with custom radiobutton

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,6 +1,3 @@
-export default function Option({id, name, value}) {
-    return (<div className="option">
-        <input type="radio" id={id} name={name} value={value} />
-        <label for={id}>{value}</label>
-    </div>)
+export default function Option({id, name, value, selected}) {
+    return (<button className={`option ${selected ? "selected" : ""}`}>Adios</button>)
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,9 +3,10 @@ import Option from "./Option"
 export default function Quiz() {
     return (<div className="quiz">
         <h3>How would one say goodbye in Spanish?</h3>
-        <Option id="quiz1" name="test" value="HTML"/>
+        <Option id="quiz1" name="test" value="HTML" selected={true}/>
         <Option id="quiz2" name="test" value="CSS"/>
         <Option id="quiz3" name="test" value="JavaScript"/>
         <Option id="quiz4" name="test" value="Java"/>
+        <hr className="quiz--divider"/>
     </div>)
 }
