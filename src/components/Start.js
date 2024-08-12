7177f7522aaa9fee9b16a4a81864f70a8cf2a508
HemantAntony <hemantantony@outlook.com>
2024-08-12 18:43:03 +0530

    Made start screen

---
+++ src/components/Start.js
@@ -0,0 +1,7 @@
+export default function Start() {
+    return (<div className="start">
+        <h1 className="start--title">Quizzical</h1>
+        <h2 className="start--description">A quiz game</h2>
+        <button className="start--button">Start quiz</button>
+    </div>)
+}
