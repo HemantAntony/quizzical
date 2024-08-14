b7b1a3ebaf3a0a2683ccaddc6dcf01f06ae23b0c
HemantAntony <hemantantony@outlook.com>
2024-08-14 19:48:00 +0530

    Implemented basic layout
    Options not visible

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,17 +2,13 @@ import Option from "./Option"
 
 export default function Question({questionId, question, value, selectedIndex}) {
 
-    const values = value.map((option, index) => {
-        return <Option value={option} selected={index === selectedIndex}/>
+    const values = value.forEach((option, index) => {
+        return <Option key={index} value={option} selected={index === selectedIndex}/>
     })
     
     return (<div>
         <h3 className="quiz--question">{question}</h3>
         {values}
-        {/* <Option value={value[0]} selected={true}/>
-        <Option value={value[1]}/>
-        <Option value={value[2]}/>
-        <Option value={value[3]}/> */}
         <hr className="quiz--divider"/>
     </div>)
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,42 @@
+import React from "react"
 import Question from "./Question"
 
 export default function Quiz() {
+    const [questions, setQuestions] = React.useState([])
+
+    
+    React.useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=10&type=multiple")
+        .then(res => res.json())
+        .then(data => {
+            if (!data || !data.results) {
+                return
+            }
+
+            console.log(data.results.map((question, index) => {
+                return <Question
+                key={index}
+                questionId={index}
+                question={question.question}
+                value={[...question.incorrect_answers, question.correct_answer]}
+                />
+            }))
+
+            setQuestions(data.results.map((question, index) => {
+                return <Question
+                key={index}
+                questionId={index}
+                question={question.question}
+                value={[...question.incorrect_answers, question.correct_answer]}
+                />
+            }))
+        })
+    }, [])
+    
     return (<div className="quiz">
-        <Question questionId={0} question={"How would one say goodbye in Spanish?"} value={["HTML", "CSS", "JavaScript", "Java"]} selectedIndex={1}/>
-        {/* <Question /> */}
-        {/* <Question /> */}
-        {/* <Question /> */}
+        <div className="quiz--questions">
+            {questions}
+        </div>
         <button className="quiz--check">Check answers</button>
     </div>)
 }
