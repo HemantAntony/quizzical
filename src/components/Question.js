5eca4fcbebb44b6003b4e2c0d10fb03dbdf94823
HemantAntony <hemantantony@outlook.com>
2024-08-14 19:51:23 +0530

    Made options visible

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,9 +2,12 @@ import Option from "./Option"
 
 export default function Question({questionId, question, value, selectedIndex}) {
 
-    const values = value.forEach((option, index) => {
+    const values = value.map((option, index) => {
         return <Option key={index} value={option} selected={index === selectedIndex}/>
     })
+
+    console.log(values)
+
     
     return (<div>
         <h3 className="quiz--question">{question}</h3>
