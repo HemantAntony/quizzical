b77c0b4c968edaeea82d87c2a0c358fb6f1bef80
HemantAntony <hemantantony@outlook.com>
2024-08-14 20:57:00 +0530

    Added margins to options and made quiz scrollable

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,9 +5,6 @@ export default function Question({questionId, question, value, selectedIndex}) {
     const values = value.map((option, index) => {
         return <Option key={index} value={option} selected={index === selectedIndex}/>
     })
-
-    console.log(values)
-
     
     return (<div>
         <h3 className="quiz--question">{question}</h3>
